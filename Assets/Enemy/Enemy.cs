6a5980e745e682c8e914ad78f0a88395900cba4f
cole-451 <coleneely87@gmail.com>
2026-03-11 23:53:35 -0600

    added some enemy requirements

--- Assets/Enemy/Enemy.cs
+++ Assets/Enemy/Enemy.cs
@@ -1,14 +1,18 @@
 using UnityEngine;
+using UnityEngine.UI;
 
 public class Enemy : MonoBehaviour
 {
-    // Start is called once before the first execution of Update after the MonoBehaviour is created
+    [SerializeField] float maxHealth = 100.0f;
+    [SerializeField] Slider healthSlider;
+
+    [SerializeField] GameObject leftDamager;
+    [SerializeField] GameObject rightDamager;
     void Start()
     {
         
     }
 
-    // Update is called once per frame
     void Update()
     {
         
